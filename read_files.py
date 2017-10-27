4c10eeeab35ef629d75fe5c9f26eb65ee1cb64cd
Tangzixia <ks2337768320@163.com>
2017-10-27 21:53:03 +0800

    Update read_files.py
    可以进行自定义标签操作，注意需要提供一个map用于映射标签和类别的关系，然后定义了标签数据的one_hot编码，然后加载训练集和测试集

--- read_files.py
+++ read_files.py
@@ -1,85 +1,85 @@
-#coding=utf-8
-#定义卷积操作  
+#-*-coding=utf-8-*-
 import tensorflow as tf
-def conv_op(input_op, name, kh, kw, n_out, dh, dw):  
-    input_op = tf.convert_to_tensor(input_op)  
-    n_in = input_op.get_shape()[-1].value  
-    with tf.name_scope(name) as scope:  
-        kernel = tf.get_variable(scope+"w",  
-                                shape = [kh, kw, n_in, n_out],  
-                                dtype = tf.float32,  
-                                initializer = tf.contrib.layers.xavier_initializer_conv2d())  
-        conv = tf.nn.conv2d(input_op, kernel, (1, dh, dw, 1), padding = 'SAME')  
-        bias_init_val = tf.constant(0.0, shape = [n_out], dtype = tf.float32)  
-        biases = tf.Variable(bias_init_val, trainable = True, name = 'b')  
-        z = tf.nn.bias_add(conv, biases)  
-        activation = tf.nn.relu(z, name = scope)  
-        return activation  
-  
-#定义全连接操作  
-def fc_op(input_op, name, n_out):  
-    n_in = input_op.get_shape()[-1].value  
-    with tf.name_scope(name) as scope:  
-        kernel = tf.get_variable(scope+'w',  
-                                shape = [n_in, n_out],  
-                                dtype = tf.float32,  
-                                initializer = tf.contrib.layers.xavier_initializer())  
-        biases = tf.Variable(tf.constant(0.1, shape = [n_out], dtype = tf.float32), name = 'b')  
-        # tf.nn.relu_layer对输入变量input_op与kernel做矩阵乘法加上bias，再做RELU非线性变换得到activation  
-        activation = tf.nn.relu_layer(input_op, kernel, biases, name = scope)  
-        return activation
-      
-#定义池化层  
-def mpool_op(input_op, name, kh, kw, dh, dw):  
-    return  tf.nn.max_pool(input_op,  
-                           ksize = [1, kh, kw, 1],  
-                           strides = [1, dh, dw, 1],  
-                           padding = 'SAME',  
-                           name = name)  
-						
-#定义网络结构
-def model(input_op,logits=False,training=False):
-    # block 1 -- outputs 112x112x64  
-    conv1_1 = conv_op(input_op, name="conv1_1", kh=3, kw=3, n_out=64, dh=1, dw=1)  
-    conv1_2 = conv_op(conv1_1,  name="conv1_2", kh=3, kw=3, n_out=64, dh=1, dw=1)  
-    pool1 = mpool_op(conv1_2,   name="pool1",   kh=2, kw=2, dw=2, dh=2)  
-  
-    # block 2 -- outputs 56x56x128  
-    conv2_1 = conv_op(pool1,    name="conv2_1", kh=3, kw=3, n_out=128, dh=1, dw=1)  
-    conv2_2 = conv_op(conv2_1,  name="conv2_2", kh=3, kw=3, n_out=128, dh=1, dw=1)  
-    pool2 = mpool_op(conv2_2,   name="pool2",   kh=2, kw=2, dh=2, dw=2)  
-  
-    # # block 3 -- outputs 28x28x256  
-    conv3_1 = conv_op(pool2,    name="conv3_1", kh=3, kw=3, n_out=256, dh=1, dw=1)  
-    conv3_2 = conv_op(conv3_1,  name="conv3_2", kh=3, kw=3, n_out=256, dh=1, dw=1)  
-    conv3_3 = conv_op(conv3_2,  name="conv3_3", kh=3, kw=3, n_out=256, dh=1, dw=1)      
-    pool3 = mpool_op(conv3_3,   name="pool3",   kh=2, kw=2, dh=2, dw=2)  
-  
-    # block 4 -- outputs 14x14x512  
-    conv4_1 = conv_op(pool3,    name="conv4_1", kh=3, kw=3, n_out=512, dh=1, dw=1)  
-    conv4_2 = conv_op(conv4_1,  name="conv4_2", kh=3, kw=3, n_out=512, dh=1, dw=1)  
-    conv4_3 = conv_op(conv4_2,  name="conv4_3", kh=3, kw=3, n_out=512, dh=1, dw=1)  
-    pool4 = mpool_op(conv4_3,   name="pool4",   kh=2, kw=2, dh=2, dw=2)  
-  
-    # block 5 -- outputs 7x7x512  
-    conv5_1 = conv_op(pool4,    name="conv5_1", kh=3, kw=3, n_out=512, dh=1, dw=1)  
-    conv5_2 = conv_op(conv5_1,  name="conv5_2", kh=3, kw=3, n_out=512, dh=1, dw=1)  
-    conv5_3 = conv_op(conv5_2,  name="conv5_3", kh=3, kw=3, n_out=512, dh=1, dw=1)  
-    pool5 = mpool_op(conv5_3,   name="pool5",   kh=2, kw=2, dw=2, dh=2)  
-  
-    # flatten  
-    shp = pool5.get_shape()  
-    flattened_shape = shp[1].value * shp[2].value * shp[3].value  
-    resh1 = tf.reshape(pool5, [-1, flattened_shape], name="resh1")  
-    # fully connected  
-    fc6 = fc_op(resh1, name="fc6", n_out=4096)  
-    fc6_drop = tf.nn.dropout(fc6, keep_prob=0.5, name="fc6_drop")  
-  
-    fc7 = fc_op(fc6_drop, name="fc7", n_out=4096)  
-    fc7_drop = tf.nn.dropout(fc7, keep_prob=0.5, name="fc7_drop")  
-  
-    logits_ = fc_op(fc7_drop, name="fc8", n_out=1000)
-    y = tf.nn.softmax(logits_, name='ybar')
-    if logits:
-        return y,logits_
-    return y  
+import numpy as np
+import os
+from PIL import Image
+
+dir="test_data_std/"
+
+def getFileArr(dir):
+	result_arr=[]
+	label_list=[]
+	map={}
+	map_file_result={}
+	map_file_label={}
+	map_new={}
+	count_label=0
+	count=0
+
+	file_list=os.listdir(dir)
+	for file in file_list:
+		file_path=os.path.join(dir,file)
+
+		label=file.split(".")[0].split("_")[0]
+		map[file]=label
+		if label not in label_list:
+			label_list.append(label)
+			map_new[label]=count_label
+			count_label=count_label+1
+		img=Image.open(file_path)
+		result=np.array([])
+		r,g,b=img.split()
+
+		r_arr=np.array(r).reshape(224*224)
+		g_arr=np.array(g).reshape(224*224)
+		b_arr=np.array(b).reshape(224*224)
+		img_arr=np.concatenate((r_arr,g_arr,b_arr))
+		result=np.concatenate((result,img_arr))
+		#result=result.reshape((3,112,112))
+		result=result.reshape((224,224,3))
+		result=result/255.0
+		map_file_result[file]=result
+		result_arr.append(result)
+		count=count+1
+	for file in file_list:
+		map_file_label[file]=map_new[map[file]]
+		#map[file]=map_new[map[file]]
+	
+	ret_arr=[]
+	for file in file_list:
+		each_list=[]
+		label_one_zero=np.zeros(count_label)
+		result=map_file_result[file]
+		label=map_file_label[file]
+		#label_one_zero[label]=1.0
+		#print(label_one_zero)
+		each_list.append(result)
+		each_list.append(label)
+		ret_arr.append(each_list)
+	np.save('test_data.npy', ret_arr)
+	return ret_arr
+
+def to_categorial(y,n_classes):
+	y_std=np.zeros([len(y),n_classes])
+	for i in range(len(y)):
+		y_std[i,y[i]]=1.0
+	return y_std
+
+def load_data(train_dir="train_data.npy",
+			test_dir="test_data.npy"):
+	train_data=np.load(train_dir)
+	test_data=np.load(test_dir)
+	X_train_non,y_train_non=train_data[:,0],train_data[:,1]
+	X_test_non,y_test_non=test_data[:,0],test_data[:,1]
+
+	X_train=np.zeros([len(X_train_non),224,224,3])
+	X_test=np.zeros([len(X_test_non),224,224,3])
+	for i in range(len(X_train_non)):
+		X_train[i,:,:,:]=X_train_non[i]
+	for i in range(len(X_test_non)):
+		X_test[i,:,:,:]=X_test_non[i]
+	#y_train_non=y_train_non.tolist()
+	#y_test_non=y_test_non.tolist()
+	y_train=to_categorial(y_train_non,1000)
+	y_test=to_categorial(y_test_non,1000)	
+	return (X_train,y_train),(X_test,y_test)
